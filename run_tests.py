1330047812cd9c14ad7ca04643aad0eb11aeaeeb
marcocannici <marco.cannici@mail.polimi.it>
2020-06-20 14:06:52 +0200

    Ran test on server for supervised with cross validation

--- run_tests.py
+++ run_tests.py
@@ -21,7 +21,7 @@ def main():
     for f in range(0,5):
         for r in range(args.runs):
             print('Running: ' + str(r) + 'time and: ' + str(f) + ' fold.')
-            supervised(['--data_dir', '/home/oscar/Desktop/Exjobb/Data/ieee_supplement/Hyperspectral_Grids/{}'.format(args.dataset), '--results', 'results/{}/'.format(args.run_name),'--epochs', '2', '--lr', '0.001', '--batch_size', '10', '--fold={}'.format(f)])
+            supervised(['--data_dir', '/data/ieee_supplement/Hyperspectral_Grids/{}'.format(args.dataset), '--results', 'results/{}/'.format(args.run_name),'--epochs', '20', '--lr', '0.001', '--batch_size', '10', '--fold={}'.format(f), '--cuda', '0', '--flip_augmentation'])
 
     print('Ran all the folds.')
 
