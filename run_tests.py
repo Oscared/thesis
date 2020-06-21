b87cb1a7b50fd86b7b5418ea901103785bb8ede5
oscared <oornberg@kth.se>
2020-06-21 21:11:25 +0000

    Added better run file

--- run_tests.py
+++ run_tests.py
@@ -3,20 +3,19 @@ import argparse
 from fixmatch_hsi import main as fixmatch
 from supervised_hsi import main as supervised
 from mixup_hsi import main as mixup
+import numpy as np
 
 
 def main():
     parser = argparse.ArgumentParser(description='Main testing file for running several tests over several datasets')
     parser.add_argument('--dataset', type=str, default='Salinas',
                         help='Name of dataset to run. Salinas, PaviaU or Indian, defaults to Salinas')
-    parser.add_argument('--fold', type=int, default=-1,
-                        help='Specify one fold to run on patch datasets. Defaults to running on all.')
     parser.add_argument('--runs', type=int, default=1,
                         help='Amount of time to run on each dataset/fold. Defaults to 1.')
     parser.add_argument('--data_sampling', type=str, default='nalepa',
                         help='What kind of sampling of data to run. random, disjoint, region or patch based. Defaults to patch (nalepa).')
-    parser.add_argument('--run_name', type=str, default='mixup',
-                        help='Folder name to save all the results to. Defaults to results/fixmatch/')
+    parser.add_argument('--run_name', type=str, default='new',
+                        help='Folder name to save all the results to. Defaults to results/new/')
     parser.add_argument('--epochs', type=int, default=20,
                         help='Amount of epochs. Defaults to 20')
     parser.add_argument('--lr', type=float, default=0.001,
@@ -37,7 +36,14 @@ def main():
     else:
         data_path = '/home/oscar/Desktop/Exjobb/Data/ieee_supplement/Hyperspectral_Grids/{}'
 
-    for f in range(0,5):
+    if args.dataset == 'Indian':
+        folds = 4
+    else:
+        folds = 5
+
+    avg_acc = np.zeros(folds)
+
+    for f in range(0,folds):
         for r in range(args.runs):
             print('Running: ' + str(r) + 'time and: ' + str(f) + ' fold.')
             if args.method == 'supervised':
@@ -50,9 +56,19 @@ def main():
                 print('No method with this name')
                 results = None
             results.append(result)
+            avg_acc[f] += result['Accuracy']
+            print(avg_acc[f])
 
+    avg_acc = avg_acc/args.runs
     print('Ran all the folds.')
+    """
+    for f in range(0,folds):
+        average_acc = results[f*args.runs:f*args.runs+args.runs]['Accuracy']
+        avg_acc[f] = np.sum(average_acc)/args.runs
+    """
+    print('Average accuracy per fold: ' + str(avg_acc))
 
+    print('Total average accuracy: ' + str(np.sum(avg_acc)/len(avg_acc)))
 
 if __name__ == '__main__':
     main()
