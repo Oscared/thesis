da606d21dad4827710d3dd7597781005f2229cf2
marcocannici <marco.cannici@mail.polimi.it>
2020-06-26 12:15:43 +0200

    Run on new cutout

--- run_tests.py
+++ run_tests.py
@@ -114,4 +114,4 @@ if __name__ == '__main__':
     """
     cutout = ['spatial', 'spectral']
     for c in cutout:
-        main(['--runs', str(3), '--cutout', c, '--epochs', '10'])
+        main(['--server', '--runs', str(3), '--cutout', c, '--epochs', '10'])
